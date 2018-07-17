509fa8d33f086359466f21076485a1dcb9120f94
LeslieInostroza <leslie.ino@gmail.com>
2018-07-17 01:53:17 -0400

    publica publicamente elimina y no se duplica

--- public/js/post.js
+++ public/js/post.js
@@ -3,10 +3,34 @@ const processPostInput = () => {
   const postInput = document.getElementById('postInput');
   firebase.database().ref().child('posts').push({
    creator : firebase.auth().currentUser.displayName,
-   text: postInput.value
+   text: postInput.value,
+   counter: counter
   });
   postInput.value = '';
 };
+
+
+
+
+
+const drawPosts = (snapshot) => {
+  let posting = '';
+  Object.entries(snapshot.val()).forEach((post) => {
+    posting += `
+    <ul class="list-group list-group-flush">
+     <li class="list-group-item">
+       <h6 class='card-title'>${post[1].creator}</h6>
+       <p class='card-text text-justify editPost'>${post[1].text}</p>
+       <i class="fas fa-trash-alt" id ="eliminarPost" data-postId="${post[0]}" onclick="deletePost(event)"></i>
+       <i class="fas fa-edit" id="editPost" data-editPost="${post[1].text}" onclick="editPosts(event)"></i>
+       <p id="likePost"><input type="button" id="botonlike" value="+1" onClick="like(event)" data-likePost="${post[0]}">0</p>
+     </li>
+    </ul>
+    `;
+  });
+  document.getElementById('postScreen').innerHTML = posting;
+};
+
 
 //borrar post
 const deletePost = (event) => {
@@ -20,26 +44,29 @@ let counter = 1;
 const like = (event) => {
   event.stopPropagation();
   const idPostsLike = event.target.getAttribute('data-likePost');
-  const refPosts = firebase.database().ref('posts').child(idPostsLike);
+  firebase.database().ref('posts').child(idPostsLike);
   document.getElementById('likePost').innerHTML = counter++;
-}
-
-
-
-const drawPosts = (snapshot) => {
-  let posting = '';
-  Object.entries(snapshot.val()).forEach((post) => {
-    posting += `
-    <ul class="list-group list-group-flush">
-     <li class="list-group-item">
-       <h6 class='card-title'>${post[1].creatorName}</h6>
-       <p class='card-text text-justify editPost'>${post[1].text}</p>
-       <i class="fas fa-trash-alt" id ="eliminarPost" data-postId="${post[0]}" onclick="deletePost(event)"></i>
-       <i class="fas fa-edit" id="editPost" data-editPost="" onclick=""></i>
-       <p id="likePost"><input type="button" id="botonlike" value="+1" onClick="like(event)" data-likePost="${post[1].key}">0</p>
-     </li>
-    </ul>
-    `;
-  });
-  document.getElementById('postScreen').innerHTML = posting;
 };
+
+//editar post
+
+/*function editPosts(event){
+  document.getElementById('postInput').value = postInput.value;
+  const btnEdit = document.getElementById('sendButtonPost');
+  btnEdit.innerHTML = 'editar';
+  
+  btnEdit.onclick = function() {
+    let postingText = document.getElementById('postInput').value;
+    const editText = event.target.getAttribute('data-editPost');
+    return firebase.database().ref('posts').child(editText).update({
+      text: postingText
+    }).then(function() {
+      console.log('editado');
+      btnEdit.innerHTML ='Publicar';
+    })
+    .catch(function(error){
+      console.error('error',error);
+    });
+  };
+};*/
+
