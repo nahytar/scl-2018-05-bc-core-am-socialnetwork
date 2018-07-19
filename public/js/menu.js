35f7b4c2bd44382cb35ab1df15fe9a4b038bbff0
nahytar <nahytar@gmail.com>
2018-07-19 13:10:00 -0400

    Se conectan botones con menu.

--- public/js/menu.js
+++ public/js/menu.js
@@ -10,19 +10,37 @@ const hideAll = () => {
   document.getElementById('imgLogo').style.display = 'none';
   document.getElementById('postArea').style.display = 'none';
   document.getElementById('profileArea').style.display = 'none';
+  document.getElementById('sectioncalendario').style.display = 'none';
+  document.getElementById('sectionreunion').style.display = 'none';
+  document.getElementById('sectionautorizacion').style.display = 'none';
+  document.getElementById('sectionactividades').style.display = 'none';
 }
 
 const showChat = () => {
   hideAll();
   document.getElementById("chatArea").style.display = "block";
 }
-
 const showPost = () => {
   hideAll();
   document.getElementById('postArea').style.display = 'block';
 }
-
 const showProfile = () => {
   hideAll();
   document.getElementById('profileArea').style.display = 'block';
+}
+const showCalendario = () => {
+  hideAll();
+  document.getElementById('sectioncalendario').style.display = 'block';
+}
+const showReunion = () => {
+  hideAll();
+  document.getElementById('sectionreunion').style.display = 'block';
+}
+const showAutorizacion = () => {
+  hideAll();
+  document.getElementById('sectionautorizacion').style.display = 'block';
+}
+const showActividades = () => {
+  hideAll();
+  document.getElementById('sectionactividades').style.display = 'block';
 }
